a048ae3142c22b1a4a2fada8fb52dad4654caab0
r4phael <albus.zly@gmail.com>
2021-01-21 10:46:58 +0800

    Use partName instead of ID in lang file

--- src/main/java/vexatos/tgregworks/item/ItemTGregPart.java
+++ src/main/java/vexatos/tgregworks/item/ItemTGregPart.java
@@ -51,7 +51,7 @@ public class ItemTGregPart extends CraftingItem implements IToolPart {
 			matName = Materials.get(data.getString("material")).mLocalizedName;
 		}
 
-		String name = StatCollector.translateToLocal("tgregworks.toolpart." + type.getID());
+		String name = StatCollector.translateToLocal("tgregworks.toolpart." + type.getPartName().replace(" ", "_"));
 		matName = name.replaceAll("%%material", matName);
 
 		if(stack.getItemDamage() == 0) {
