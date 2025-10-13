044414271e9c1db8368551e08ce479e59dbf7730
Julia <78276563+Vlamonster@users.noreply.github.com>
2025-10-13 16:50:12 +0200

    Update for Material Builder PR  (#19)

--- src/main/java/vexatos/tgregworks/util/TGregUtils.java
+++ src/main/java/vexatos/tgregworks/util/TGregUtils.java
@@ -47,7 +47,7 @@ public class TGregUtils {
 	public static ItemStack newItemStack(Materials m, PartTypes p, int amount) {
 		ItemStack stack = new ItemStack(TGregworks.registry.toolParts.get(p), amount, TGregworks.registry.matIDs.get(m));
 		NBTTagCompound data = TGregUtils.getTagCompound(stack);
-		data.setString("material", m.name());
+		data.setString("material", m.getName());
 		stack.setTagCompound(data);
 		return stack;
 	}
